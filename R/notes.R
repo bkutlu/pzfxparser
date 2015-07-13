6da806ab7044083e3f0afae1af938eb9f6bf2107
bkutlu <bkutluisb@gmail.com>
2015-07-13 10:22:32 -0700

    corrected testthat

---
+++ R/notes.R
@@ -0,0 +1,11 @@
+# imports
+devtools::use_package("XML")
+devtools::use_package("methods")
+devtools::use_vignette("package-use")
+# testing
+devtools::use_estthat()
+library(testthat)
+string <- "Testing is fun!"
+expect_match(string, "Testing")
+
+# i added new stuff
